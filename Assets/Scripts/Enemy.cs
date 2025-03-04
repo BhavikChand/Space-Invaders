289b8b2777932764fc907ea56600e149bc461731
BhavikChand <hyruka98@gmail.com>
2025-03-03 16:24:24 -0800

    First Lecture Implementation finished
    Finished the first lecture's implementation

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -4,9 +4,21 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    public delegate void EnemyDied(int points); //This defines a type not a function.
+
+    public static event EnemyDied OnEnemyDied; //static, this belongs to the class not each individual enemy. Property of the class itself not each instance of the class.
+    
     // Start is called before the first frame update
     void OnCollisionEnter2D(Collision2D collision)
     {
       Debug.Log("Ouch!");
+      Destroy(collision.gameObject);
+
+      // OnEnemyDied?.Invoke(); Is the same as the if statement.
+      if (OnEnemyDied != null)
+      {
+          OnEnemyDied.Invoke(3); //Change me later
+      }
+      
     }
 }
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,22 +1,41 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class Player : MonoBehaviour
 {
-  public GameObject bullet;
+  [FormerlySerializedAs("bullet")]
+  public GameObject bulletPrefab;
 
   public Transform shottingOffset;
-    // Update is called once per frame
+
+  void Start()
+  {
+    Enemy.OnEnemyDied += EnemyOnOnEnemyDied; //At start we sign up to the OnEnemyDied
+  }
+
+  private void OnDestroy()
+  {
+    Enemy.OnEnemyDied -= EnemyOnOnEnemyDied; //If the player died, destroy the subscription observer pattern. 
+  }
+
+  private void EnemyOnOnEnemyDied(int points) //Looks like the delegate
+  {
+    //I want to know about points here.
+    Debug.Log($"I know about the dead enemy... points:{points}");
+  }
+
+  // Update is called once per frame
     void Update()
     {
       if (Input.GetKeyDown(KeyCode.Space))
       {
-        GameObject shot = Instantiate(bullet, shottingOffset.position, Quaternion.identity);
+        GameObject shot = Instantiate(bulletPrefab, shottingOffset.position, Quaternion.identity);
         Debug.Log("Bang!");
 
-        Destroy(shot, 3f);
-
+        // Destroy(shot, 3f);
       }
     }
 }
